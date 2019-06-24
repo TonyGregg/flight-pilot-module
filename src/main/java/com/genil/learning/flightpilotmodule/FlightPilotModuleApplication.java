40281377b4b03c519f0d737eeed2bf3bb1a8b58a
TonyGregg <antonygenil@gmail.com>
2019-06-23 22:52:23 -0500

    Initial commit

---
+++ src/main/java/com/genil/learning/flightpilotmodule/FlightPilotModuleApplication.java
@@ -0,0 +1,13 @@
+package com.genil.learning.flightpilotmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPilotModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPilotModuleApplication.class, args);
+	}
+
+}
